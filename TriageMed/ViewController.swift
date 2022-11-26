7a3820c9c744e30e9e4028707f56980b2941eba9
ankitgusai <ankitgusai19@gmail.com>
2022-11-26 17:12:49 -0500

    SKeleton app

---
+++ TriageMed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriageMed
+//
+//  Created by Ankit Gusai on 26/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
